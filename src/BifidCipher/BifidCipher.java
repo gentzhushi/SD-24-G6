cbc32436b5dd95d9032c73f9bde95733239d7479
Gerta Hodolli <gerta.hodolli@student.uni-pr.edu>
2024-04-21 11:32:55 +0200

    Add initial class structure for Bifid Cipher implementation

---
+++ src/BifidCipher/BifidCipher.java
@@ -0,0 +1,19 @@
+package BifidCipher;
+
+class PolybiusSquare {
+
+}
+
+class BifidCipherEncrypt extends PolybiusSquare {
+
+}
+
+class BifidCipherDecrypt extends PolybiusSquare {
+
+}
+
+// Klasa kryesore që përdor kodimin dhe dekodimin
+public class BifidCipher {
+    public static void main(String[] args) {
+    }
+}
