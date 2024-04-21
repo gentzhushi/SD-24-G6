6a09da7c2f928ea8f69798db1df3a5381a25c32b
Gent Zhushi <gentzhushi01@gmail.com>
2024-04-21 23:14:34 +0200

    failed attemt per me kry pun
    kodi i vertete do te gjithndet ne nje repository tjter.

--- src/PermutationCipher/PermutationCipher.java
+++ src/PermutationCipher/PermutationCipher.java
@@ -1,4 +1,19 @@
 package PermutationCipher;
 
+import  org.python.util.PythonInterpreter;
+
 public class PermutationCipher {
+    public static void main(String[] args){
+        String fajlli = "src/Python/main.py";
+        try{
+            PythonInterpreter py = new PythonInterpreter();
+            py.exec("import warnings");
+            py.execfile(fajlli);
+            py.cleanup();
+            py.close();
+        } catch (Exception e){
+            System.err.println("Gabim gjate eksekutimit te programit!");
+            System.err.println(e.getMessage());
+        }
+    }
 }
