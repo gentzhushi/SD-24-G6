5b7a1c61ee44ba9551376c405fca22eb1d13d885
Gerta Hodolli <gerta.hodolli@student.uni-pr.edu>
2024-04-21 12:36:07 +0200

    Update BifidCipher.java
    Implement Polybius Square for Bifid Cipher with Coordinate Mapping

--- src/BifidCipher/BifidCipher.java
+++ src/BifidCipher/BifidCipher.java
@@ -1,7 +1,23 @@
 package BifidCipher;
 
-class PolybiusSquare {
+import java.util.HashMap;
 
+class PolybiusSquare {
+    protected final char[][] SQUARE = {
+            {'a', 'b', 'c', 'd', 'e'},
+            {'f', 'g', 'h', 'i', 'k'},
+            {'l', 'm', 'n', 'o', 'p'},
+            {'q', 'r', 's', 't', 'u'},
+            {'v', 'w', 'x', 'y', 'z'}
+    };
+    protected final HashMap<Character, int[]> charToCoord = new HashMap<>();
+    public PolybiusSquare() {
+        for (int i = 0; i < SQUARE.length; i++) {
+            for (int j = 0; j < SQUARE[i].length; j++) {
+                charToCoord.put(SQUARE[i][j], new int[]{i + 1, j + 1});
+            }
+        }
+    }
 }
 
 class BifidCipherEncrypt extends PolybiusSquare {
